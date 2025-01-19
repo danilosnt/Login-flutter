5d7351797b5e93d7e31cddfadefbc2c760a923cd
danilosnt <daniloddocarmo16@icloud.com>
2025-01-19 20:14:06 -0300

    alteração mensagem principal

--- lib/main.dart
+++ lib/main.dart
@@ -112,7 +112,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Você apertou o botão nessa quantidade de vezes:',
             ),
             Text(
               '$_counter',
